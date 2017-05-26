0cbf269821b1374e36a34fc1f49abb699f404bb6
harsh <harsh.mistry@majesco.com>
2017-05-25 20:34:38 -0400

    connected login with signup page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  toggleDetailPage: boolean;
   constructor() { }
 
   ngOnInit() {
+    this.toggleDetailPage = false;
   }
 
 }
